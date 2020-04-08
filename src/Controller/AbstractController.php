4de51267030edb470019ff253e443b062c9592c5
Andreea Sabadis <andreea.sabadis@evozon.com>
2020-04-08 14:15:50 +0300

    Add createRedirectResponse() function to AbstractController

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace Framework\Controller;
 
 use Framework\Contracts\RendererInterface;
+use Framework\Http\Response;
+use Framework\Http\Stream;
 
 /**
  * Base abstract class for application controllers.
@@ -20,5 +22,15 @@ abstract class AbstractController
     {
         // Rendered gets constructor injected
         $this->renderer = $renderer;
+    }
+
+    public function createRedirectResponse(string $location): Response
+    {
+        $body = Stream::createFromString('');
+        /** @var $response Response */
+        $response = new Response($body, '1.1', 301, '');
+        $response = $response->withHeader('Location', $location);
+
+        return $response;
     }
 }
